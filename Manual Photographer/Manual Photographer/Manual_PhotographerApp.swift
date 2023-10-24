f8d25bc5933200927e6648b5dee13f88e84e365d
Makio <45434556+2MikiMaki2@users.noreply.github.com>
2023-10-24 09:07:42 -0700

    Initial files

---
+++ Manual Photographer/Manual Photographer/Manual_PhotographerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manual_PhotographerApp.swift
+//  Manual Photographer
+//
+//  Created by Maksim on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Manual_PhotographerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
