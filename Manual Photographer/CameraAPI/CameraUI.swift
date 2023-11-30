f02f88bee1ee006beea83e47dc89b6eae0905d1e
Maksim Pavlicic <maksimpavlicic@gmail.com>
2023-11-29 22:20:59 -0800

    Skeleton code for capturing photo, also added button for taking photo

---
+++ Manual Photographer/CameraAPI/CameraUI.swift
@@ -0,0 +1,18 @@
+//
+//  CameraUI.swift
+//  Manual Photographer
+//
+//  Created by Maksim on 11/29/23.
+//
+
+import SwiftUI
+
+struct CameraUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraUI()
+}
---
+++ Manual Photographer/CameraAPI/CapturePhotoDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  CapturePhotoDelegate.swift
+//  Manual Photographer
+//
+//  Created by Maksim on 11/29/23.
+//
+
+import Foundation
